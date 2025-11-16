0ff74584e33cec23203ea581ed09071ae47e803e
smaslan <s.maslan@seznam.cz>
2025-11-16 14:55:38 +0100

    fs archive now supports delayed data loading on request

--- source/fs_archive.cpp
+++ source/fs_archive.cpp
@@ -17,7 +17,7 @@
 using namespace std;
 
 // load entire archive file
-FSarchive::FSarchive(wstring &path, int options)
+FSarchive::FSarchive(wstring path, int options)
 {
 	m_lzw = NULL;
 	m_fs_name = std::filesystem::path(path).filename().string();
@@ -25,14 +25,16 @@ FSarchive::FSarchive(wstring &path, int options)
 }
 
 // load more data to FS archive object
-void FSarchive::Append(wstring& path,int options)
+void FSarchive::Append(wstring path,int options)
 {
 	// try to open FS archive
+	m_file_path = path;
 	ifstream fr(path,ios::in | ios::binary);
 	if(!fr)
 		throw runtime_error(string_format("Cannot open \"%ls\"!",path.c_str()));
 
 	// init LZW decoder?
+	m_options = options;
 	if(options & Options::DELZ_ALL && !m_lzw)
 	{
 		m_lzw = new LZWexpand(1000000);
@@ -66,27 +68,42 @@ void FSarchive::Append(wstring& path,int options)
 		uint32_t aflen;
 		fr.read((char*)&aflen,sizeof(uint32_t));
 
-		// copy file data to buffer		
-		file->data.resize(aflen);
-		streampos next = fr.tellg();
-		fr.seekg(afofs);
-		fr.read((char*)file->data.data(),aflen);
-		fr.seekg(next);
+		// file position and size on FS
+		file->file_size = aflen;
+		file->file_pos = afofs;
 
-		// optional deLZ of known types
-		if(m_lzw)
+		if(options & FSarchive::Options::NO_LOAD)
 		{
-			std::string ext = std::filesystem::path(name).extension().string();
-			if(options & Options::DELZ_LZ && ext.compare(".LZ") == 0 
-				|| options & Options::DELZ_LZ0 && ext.compare(".LZ0") == 0)
-			{				
-				auto &data = m_lzw->Decode(&file->data[0],&file->data[0] + aflen);
-				if(data.empty())
-				{
-					delete file;
-					throw std::runtime_error(string_format("DeLZ of file \"%s\" failed!",name));
+			// no load data mode - just store file params:
+			file->is_loaded = false;
+		}
+		else
+		{
+			// load data mode:
+			file->is_loaded = true;
+			
+			// copy file data to buffer		
+			file->data.resize(aflen);
+			streampos next = fr.tellg();
+			fr.seekg(afofs);
+			fr.read((char*)file->data.data(),aflen);
+			fr.seekg(next);
+
+			// optional deLZ of known types
+			if(m_lzw)
+			{
+				std::string ext = std::filesystem::path(name).extension().string();
+				if(options & Options::DELZ_LZ && ext.compare(".LZ") == 0 
+					|| options & Options::DELZ_LZ0 && ext.compare(".LZ0") == 0)
+				{				
+					auto &data = m_lzw->Decode(&file->data[0],&file->data[0] + aflen);
+					if(data.empty())
+					{
+						delete file;
+						throw std::runtime_error(string_format("DeLZ of file \"%s\" failed!",name));
+					}
+					file->data = data;				
 				}
-				file->data = data;				
 			}
 		}
 
@@ -96,6 +113,79 @@ void FSarchive::Append(wstring& path,int options)
 
 	// close file
 	fr.close();
+}
+
+// load file data for given file record if not loaded before
+int FSarchive::LoadFile(FSfile *fsfile)
+{
+	if(fsfile->is_loaded)
+		return(0);
+
+	// try open FS archive
+	ifstream fr(m_file_path,ios::in | ios::binary);
+	if(!fr)
+		return(1);
+
+	// load file data to buffer
+	fsfile->data.resize(fsfile->file_size);	
+	fr.seekg(fsfile->file_pos);
+	fr.read((char*)fsfile->data.data(),fsfile->file_size);
+	fr.close();
+	
+	// optional deLZ of known types
+	if(m_lzw)
+	{
+		std::string ext = std::filesystem::path(fsfile->name).extension().string();
+		if(m_options & Options::DELZ_LZ && ext.compare(".LZ") == 0
+			|| m_options & Options::DELZ_LZ0 && ext.compare(".LZ0") == 0)
+		{
+			auto& data = m_lzw->Decode(&fsfile->data[0],&fsfile->data[0] + fsfile->file_size);
+			if(data.empty())
+				return(1);
+			fsfile->data = data;
+		}
+	}
+
+	// mark file as loaded
+	fsfile->is_loaded = true;
+
+	return(0);
+}
+
+// free loaded file data (will be reloaded on next access)
+int FSarchive::FreeFileData(FSfile* fsfile)
+{
+	if(!fsfile)
+		return(1);
+	fsfile->data.clear();
+	fsfile->is_loaded = false;
+	return(0);
+}
+
+// free loaded file data (will be reloaded on next access)
+int FSarchive::FreeFileData(std::string name)
+{
+	return(FreeFileData(name.c_str()));
+}
+// free loaded file data (will be reloaded on next access)
+int FSarchive::FreeFileData(const char *name)
+{
+	for(auto& file : m_files)
+	{
+		if(_strcmpi(file->name.c_str(),name) == 0)
+		{
+			FreeFileData(file);
+			return(0);
+		}
+	}
+	return(1);
+}
+// free loaded file data (will be reloaded on next access)
+int FSarchive::FreeFileData(int id)
+{
+	if(id < 0 || id >= m_files.size())
+		return(1);
+	return(FreeFileData(m_files[id]));
 }
 
 
@@ -133,6 +223,14 @@ int FSarchive::GetFile(const char* name, uint8_t** data, int* size)
 	{
 		if (_strcmpi(file->name.c_str(), name) == 0)
 		{
+			if(!file->is_loaded)
+			{
+				// load file data first
+				if(LoadFile(file))
+					return(1);
+			}
+			
+			// file loaded - just return
 			if(data)
 				*data = file->data.data();
 			if(size)
@@ -153,7 +251,8 @@ string FSarchive::GetFile(const char* name)
 	string text;
 	uint8_t* data;
 	int size;
-	GetFile(name, &data, &size);
+	if(GetFile(name, &data, &size))
+		return(text);
 	text.resize(size);
 	memcpy(&text[0],data,size);
 	return(text);
@@ -163,6 +262,8 @@ string FSarchive::GetFile(const char* name)
 int FSarchive::GetFile(int id, uint8_t** data, int* size, const char **name)
 {	
 	if (id >= m_files.size())
+		return(1);
+	if(LoadFile(m_files[id]))
 		return(1);
 	if (data)
 		*data = m_files[id]->data.data();
@@ -188,4 +289,14 @@ std::string FSarchive::GetFSname(bool with_extension)
 		return(m_fs_name);
 	else
 		return(std::filesystem::path(m_fs_name).replace_extension().string());
+}
+
+// get wildcard filtered file names
+std::vector<std::string> FSarchive::GetFileNames(std::string wild)
+{
+	std::vector<std::string> list;
+	for(auto &file: m_files)
+		if(wildcmp(wild.c_str(),file->name.c_str()))
+			list.push_back(file->name);
+	return(list);
 }
--- source/fs_archive.h
+++ source/fs_archive.h
@@ -24,17 +24,21 @@ class FSarchive
 			static constexpr int DELZ_LZ = 0x01;
 			static constexpr int DELZ_LZ0 = 0x02;
 			static constexpr int DELZ_ALL = DELZ_LZ | DELZ_LZ0;
+			static constexpr int NO_LOAD = 0x04; // do not load files data
 		};
 
 		class FSfile
 		{
 		public:
+			bool is_loaded;
+			std::streampos file_pos;
+			uint32_t file_size;
 			std::string name;
 			std::vector<uint8_t> data;
 		};		
 
-		FSarchive(std::wstring &path, int options=Options::NONE);
-		void Append(std::wstring& path,int options=Options::NONE);
+		FSarchive(std::wstring path, int options=Options::NONE);
+		void Append(std::wstring path,int options=Options::NONE);
 		~FSarchive();		
 		std::vector<FSfile*> &GetFiles();
 		int GetFile(const char* name, uint8_t** data, int* size);
@@ -42,14 +46,22 @@ class FSarchive
 		int GetFile(int id, uint8_t** data, int* size, const char** name=NULL);		
 		inline std::string GetFile(const char* name);
 		std::string GetFile(std::string& name);
+		int FreeFileData(const char* name);
+		int FreeFileData(std::string name);
+		int FreeFileData(int id);
 		int Count();
 		const char *GetFileName(int id);
 		std::string GetFSname(bool with_extension=true);
+		std::vector<std::string> GetFileNames(std::string wild="*");
 
 	private:			
 		
+		int m_options;
+		std::wstring m_file_path;
 		std::string m_fs_name;
 		LZWexpand* m_lzw;
 		std::vector<FSfile*> m_files;
+		int LoadFile(FSfile* fsfile);
+		int FreeFileData(FSfile* fsfile);
 };
 
